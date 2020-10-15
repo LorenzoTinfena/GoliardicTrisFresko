1c4b6370bc309e8b5f8566a45cb5b1adc50eb6b8
Lorenzo Tinfena <lorenzo.tinfena.personal@gmail.com>
2020-10-15 09:23:35 +0200

    add some cool things

--- app/src/main/java/com/lorenzotinfena/goliardictrisfresko/Game.java
+++ app/src/main/java/com/lorenzotinfena/goliardictrisfresko/Game.java
@@ -1,24 +1,43 @@
 package com.lorenzotinfena.goliardictrisfresko;
 
 public class Game {
-    private final Cell[][] cells = new Cell[3][3];
-    public boolean move(int i, int j, boolean isCross)
+    public final Cell[][] cells = new Cell[3][3];
+    public boolean move(int i, int j, Cell cell) //contraint cell != Cell.Empty
     {
-        if (isCross)
+        if (cell == Cell.Cross)
             cells[i][j] = Cell.Cross;
         else
             cells[i][j] = Cell.Nought;
 
-        return isVictory();
+        return isVictory(cell);
     }
-    private boolean isVictory()
+    private boolean isVictory(Cell cell)
     {
+        int[][] lati = new int[2][3];
+        int[] diagonali = new int[2];
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                if ()//TOO
+                if (cells[i][j] == cell)
+                {
+                    lati[0][i]++;
+                    lati[1][j]++;
+                    if (i == j)
+                        diagonali[0]++;
+                    if (i+j == 2)
+                        diagonali[1]++;
+                }
             }
         }
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (lati[0][i] == 3 || lati[1][i] == 3)
+                return true;
+        }
+        if (diagonali[0] == 3 || diagonali[1] == 3)
+            return true;
+        return false;
     }
 }
--- app/src/main/java/com/lorenzotinfena/goliardictrisfresko/MainActivity.java
+++ app/src/main/java/com/lorenzotinfena/goliardictrisfresko/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     private final ImageButton[][] btns = new ImageButton[3][3];
     private Game game = new Game();
@@ -17,17 +19,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // assign buttons
-        btns[0][0] = findViewById(R.id.ImageButton00);
-        btns[1][0] = findViewById(R.id.ImageButton10);
-        btns[2][0] = findViewById(R.id.ImageButton20);
-        btns[0][1] = findViewById(R.id.ImageButton01);
-        btns[1][1] = findViewById(R.id.ImageButton11);
-        btns[2][1] = findViewById(R.id.ImageButton21);
-        btns[0][2] = findViewById(R.id.ImageButton02);
-        btns[1][2] = findViewById(R.id.ImageButton12);
-        btns[2][2] = findViewById(R.id.ImageButton22);
 
-        game.move(0, 0, true);
+        this.btns[0][0] = findViewById(R.id.ImageButton00);
+        this.btns[1][0] = findViewById(R.id.ImageButton10);
+        this.btns[2][0] = findViewById(R.id.ImageButton20);
+        this.btns[0][1] = findViewById(R.id.ImageButton01);
+        this.btns[1][1] = findViewById(R.id.ImageButton11);
+        this.btns[2][1] = findViewById(R.id.ImageButton21);
+        this.btns[0][2] = findViewById(R.id.ImageButton02);
+        this.btns[1][2] = findViewById(R.id.ImageButton12);
+        this.btns[2][2] = findViewById(R.id.ImageButton22);
+
+        for (final int i = 0; i < 3; i++)
+        {
+            for (final int j = 0; j < 3; j++)
+            {
+                this.btns[i][j].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        btn_click(i, j, view);
+                    }
+                });
+            }
+        }
+
+        findViewById(R.id.btn_reset).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
     }
 
     @Override
@@ -52,4 +73,41 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private final int[] crosses = {R.drawable.C0, R.drawable.C1, R.drawable.C2, R.drawable.C3};
+    private final int[] noughts = {R.drawable.n0, R.drawable.n1, R.drawable.n2, R.drawable.n3};
+    private int getRandomDrawableRes(Cell cell)
+    {
+        Random ran = new Random();
+        if (cell == Cell.Cross)
+            return crosses[ran.nextInt(crosses.length)];
+        else
+            return noughts[ran.nextInt(noughts.length)];
+    }
+
+    private Cell turno_attuale = Cell.Cross;
+    private void btn_click(int i, int j, View view)
+    {
+        if (this.game.cells[i][j] == Cell.Empty)
+        {
+            ((ImageButton)view).setImageResource(R.drawable.);
+            if (game.move(i, j, turno_attuale))
+                mostra_vittoria(turno_attuale);
+
+            //change turn
+            if (this.turno_attuale == Cell.Cross)
+                this.turno_attuale = Cell.Nought;
+            else
+                this.turno_attuale = Cell.Cross;
+        }
+    }
+    private void mostra_vittoria(Cell cell)
+    {
+        //TODO
+        reset();
+    }
+    private void reset()
+    {
+        game = new Game();
+
+    }
 }
